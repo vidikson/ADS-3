93018f0740ae8214385eb240cfda8adbe292cf82
vidikson <65093952+vidikson@users.noreply.github.com>
2021-04-25 21:51:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,9 +3,11 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-    
+  // добавьте сюда нужный код
+  return std::string("");
 }
 
 int eval(std::string pst) {
-   
+  // добавьте сюда нужный код
+  return 0;
 }
