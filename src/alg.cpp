1bcc11e0d287d233c640b8c8e10d91304e3bc81e
vidikson <65093952+vidikson@users.noreply.github.com>
2021-05-09 00:26:13 +0300

    Кутузова Виктория

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,43 +3,44 @@
 #include "tstack.h"
 
 int priority(char a) {
-    if (a == '*' || a == '/') {
-        return 3;
-    }
-    else {
-        if (а == '+' || a == '-') {
-            return 2;
-        }
-        else if (a == ')')
-            return 1;
-        else
-            return 0;
+    switch (a) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    default:return -1;
     }
 }
 
 std::string infx2pstfx(std::string inf) {
-  TStack<char>stack1;
+  TStack<char> stack1;
     std::string str;
     for (int i = 0; i < inf.length(); i++) {
         if ((inf[i] >= '0') && (inf[i] <= '9')) {
             str += inf[i];
             str += ' ';
-        }
-        else if ((inf[i] == '(') || (priority(inf[i]) > priority(stack1.get())) || (stack1.isEmpty())) {
+        } 
+        else if ((inf[i] == '(')
+                 || (priority(inf[i]) > priority(stack1.get()))
+                 || (stack1.isEmpty())) {
             stack1.push(inf[i]);
-        }
+        } 
         else if (inf[i] == ')') {
-            while (!stack1.isEmpty() && stack1.get() != '(') {
-                str += stack1.get();
+            char s = stack1.get();
+            stack1.pop();
+            while (s != '(') {
+                str += s;
                 str += ' ';
-                stack1.pop();
+              s = stack1.get();
+              stack1.pop();
             }
-            if (stack1.get() == '(')
-                stack1.pop();
-        }
+        } 
         else {
-            while ((!stack1.isEmpty()) && (priority(stack1.get()) >= priority(inf[i]))) {
-                str = stack1.get();
+            while ((!stack1.isEmpty())
+                   && (priority(stack1.get()) >= priority(inf[i]))) {
+                str += stack1.get();
                 str += ' ';
                 stack1.pop();
             }
@@ -51,36 +52,36 @@ std::string infx2pstfx(std::string inf) {
                 str += ' ';
                 stack1.pop();
             }
+    str.pop_back();
     return str;
 }
 
 int eval(std::string pst) {
-   TStack<int>stack2;
-    int res;
+  TStack<int> stack2;
     for (int i = 0; i < pst.length(); i++) {
         if ((pst[i] >= '0') && (pst[i] <= '9')) {
-            stack2.push(pst[i]- '0');
-        }
-        else
-            if (pst[i] != ' ') {
+            stack2.push(pst[i] - '0');
+        } 
+        else if (pst[i] != ' ') {
                 int y = stack2.get();
                 stack2.pop();
                 int x = stack2.get();
                 stack2.pop();
                 if (pst[i] == '-') {
                     stack2.push(x - y);
-                }
+                } 
                 else if (pst[i] == '+') {
                     stack2.push(x + y);
-                }
+                } 
                 else if (pst[i] == '*') {
                     stack2.push(x * y);
-                }
+                } 
                 else {
                     stack2.push(x / y);
                 }
             }
     }
+    int res;
     res = stack2.get();
     return res;
 }
